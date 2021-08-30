068dd8bdfa4f184b6f37484af057a508746b897c
Michele Giammarini <giammarinimichele@gmail.com>
2021-08-30 16:22:18 +0200

    first commit

---
+++ Controllers/TodoesManualeController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestStorage.Models;
+
+namespace TestStorage.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoesManualeController : ControllerBase
+    {
+        private readonly AppDataContext _context;
+
+        public TodoesManualeController(AppDataContext appDataContext)
+        {
+            _context = appDataContext;
+        }
+
+        [HttpGet]
+        public List<Todo> GetTodos()
+        {
+            return _context.Todos.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public Todo GetTodo(int id)
+        {
+            var todo = _context.Todos.Find(id);
+            
+            return todo;
+        }
+
+        [HttpPost]
+        public Todo PostTodo(Todo todo)
+        {             
+            while (_context.Todos.Find(todo.Id) != null || todo.Id==0)
+            {
+                todo.Id++;
+            }
+            _context.Todos.Add(todo);
+            _context.SaveChanges();
+            return todo;
+        }
+
+        [HttpPut("{id}")]
+        public Todo UpdateTodo(int id, Todo todoUpd)
+        {
+            
+            var todo = _context.Todos.Find(id);
+            todo.Name = todoUpd.Name;
+            todo.IsComplete = todoUpd.IsComplete;
+            todo.Notes = todoUpd.Notes;
+            _context.SaveChanges();
+            return todo;
+        }
+
+        [HttpDelete("{id}")]
+        public Todo DeleteTodo(int id)
+        {
+            var todo = _context.Todos.Find(id);
+            _context.Todos.Remove(todo);
+            _context.SaveChanges();
+            return todo;
+        }
+    }
+}
---
+++ Models/AppDataContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace TestStorage.Models
+{
+    public partial class AppDataContext : DbContext
+    {
+        /*public AppDataContext()
+        {
+        }*/
+
+        public AppDataContext(DbContextOptions<AppDataContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server = LAPTOP-5HQ7N5VL\\MSSQLSERVER13; Database = supermercato; Trusted_Connection = True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Todo>(entity =>
+            {
+                entity.ToTable("todo");
+
+                entity.Property(e => e.Id)
+                    .ValueGeneratedNever()
+                    .HasColumnName("id");
+
+                entity.Property(e => e.IsComplete).HasColumnName("isComplete");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.Notes)
+                    .IsUnicode(false)
+                    .HasColumnName("notes");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
